3af49b7eaaf355a117b43892a39d3af91615615c
4stru <4.struong@kscholars.org>
2020-01-03 22:05:28 -0600

    checking fuel and mass for launch status

--- script.js
+++ script.js
@@ -26,6 +26,61 @@ window.addEventListener("load", function() {
     }
 
     // Functions
+    let fullFuelLevel = () => {
+      if (fuelLevelInput.value < 10000) {
+        event.preventDefault();
+        document.getElementById("faultyItems").style.visibility = "visible";
+        document.getElementById("fuelStatus").innerHTML =
+          "Not enough fuel for journey";
+        document.getElementById("launchStatus").innerHTML =
+          "Shuttle not ready for launch";
+        document.getElementById("launchStatus").style.color = "red";
+        return false;
+      } else {
+        document.getElementById("fuelStatus").innerHTML =
+          "Enough fuel for journey";
+        return true;
+      }
+    };
+
+    let lightCargo = () => {
+      if (cargoMassInput.value > 10000) {
+        document.getElementById("faultyItems").style.visibility = "visible";
+        document.getElementById("cargoStatus").innerHTML =
+          "Too much mass for take off";
+        document.getElementById("launchStatus").innerHTML =
+          "Shuttle not ready for launch";
+        document.getElementById("launchStatus").style.color = "red";
+        event.preventDefault();
+        return false;
+      } else {
+        document.getElementById("cargoStatus").innerHTML =
+          "Light enough for take off";
+        return true;
+      }
+    };
+
+    let readyToLaunch = () => {
+      if (lightCargo() === true && fullFuelLevel() === true) {
+        event.preventDefault();
+        document.getElementById("launchStatus").innerHTML =
+          "Shuttle ready for launch";
+        document.getElementById("launchStatus").style.color = "green";
+
+        pilotStatus.innerHTML = `${pilotNameInput.value} is ready for launch`;
+        copilotStatus.innerHTML = `${copilotNameInput.value} is ready for launch`;
+      } else {
+        event.preventDefault();
+        document.getElementById("launchStatus").innerHTML =
+          "Shuttle not ready for launch";
+        document.getElementById("launchStatus").style.color = "red";
+
+        pilotStatus.innerHTML = `${pilotNameInput.value} is not ready for launch`;
+        copilotStatus.innerHTML = `${copilotNameInput.value} is not ready for launch`;
+      }
+    };
+
+    readyToLaunch();
   });
 
   //Mission Destination Information
