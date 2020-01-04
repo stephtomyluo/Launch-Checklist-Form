67e53a2043527da1a3b6b71e72c0e0131b28e965
4stru <4.struong@kscholars.org>
2020-01-03 20:46:18 -0600

    validate text and numbers

--- script.js
+++ script.js
@@ -1,28 +1,38 @@
 // Write your JavaScript code here!
 
-   window.addEventListener("load", function() {
-      let form = document.querySelector("form");
-      form.addEventListener("submit", function(event) {
-         let pilotName = document.querySelector("input[name=pilotName]");
-         let copilotName = document.querySelector("input[name=copilotName]");
-         let fuelLevel = document.querySelector("input[name=fuelLevel]");
-         let cargoMass = document.querySelector("input[name=cargoMass]");
-         if (pilotName.value === "" || copilotName.value === "" || fuelLevel.value === "" || cargoMass.value === "") {
-            alert("All fields are required!");
-            // stop the form submission
-            event.preventDefault();
-         }
-      });
-   });
+window.addEventListener("load", function() {
+  let form = document.querySelector("form");
+  form.addEventListener("submit", function(event) {
+    let pilotName = document.querySelector("input[name=pilotName]");
+    let copilotName = document.querySelector("input[name=copilotName]");
+    let fuelLevel = document.querySelector("input[name=fuelLevel]");
+    let cargoMass = document.querySelector("input[name=cargoMass]");
+    if (
+      pilotName.value === "" ||
+      copilotName.value === "" ||
+      fuelLevel.value === "" ||
+      cargoMass.value === ""
+    ) {
+      alert("All fields are required!");
+      // stop the form submission
+      event.preventDefault();
+    }
+
+    if (isNaN(pilotName.value) == false || isNaN(copilotName.value) == false) {
+      alert("Text is required for Pilot and Co-Pilot");
+      event.preventDefault();
+    }
+  });
+});
 
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
 <ol>
-   <li>Name: ${}</li>
-   <li>Diameter: ${}</li>
-   <li>Star: ${}</li>
-   <li>Distance from Earth: ${}</li>
-   <li>Number of Moons: ${}</li>
+<li>Name: ${}</li>
+<li>Diameter: ${}</li>
+<li>Star: ${}</li>
+<li>Distance from Earth: ${}</li>
+<li>Number of Moons: ${}</li>
 </ol>
 <img src="${}">
 */
