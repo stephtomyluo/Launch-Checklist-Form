4618f2a9e7e67e7c2fb7d0cd061471739ef3fe1f
4stru <4.struong@kscholars.org>
2020-01-03 21:31:21 -0600

    fixed validation and added mission into div

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 // Write your JavaScript code here!
-
 window.addEventListener("load", function() {
+  let form = document.querySelector("form");
+
+  let pilotNameInput = document.querySelector("input[name=pilotName]");
+  let copilotNameInput = document.querySelector("input[name=copilotName]");
+  let fuelLevelInput = document.querySelector("input[name=fuelLevel]");
+  let cargoMassInput = document.querySelector("input[name=cargoMass]");
+
   let launchStatus = document.querySelector("#launchStatus");
   let faultyItems = document.querySelector("#faultyItems");
   let pilotStatus = document.querySelector("#pilotStatus");
@@ -8,38 +14,38 @@ window.addEventListener("load", function() {
   let fuelStatus = document.querySelector("#fuelStatus");
   let cargoStatus = document.querySelector("cargoStatus");
 
-  let form = document.querySelector("form");
   form.addEventListener("submit", function(event) {
-    let pilotName = document.querySelector("input[name=pilotName]");
-    let copilotName = document.querySelector("input[name=copilotName]");
-    let fuelLevel = document.querySelector("input[name=fuelLevel]");
-    let cargoMass = document.querySelector("input[name=cargoMass]");
     if (
-      pilotName.value === "" ||
-      copilotName.value === "" ||
-      fuelLevel.value === "" ||
-      cargoMass.value === ""
+      pilotNameInput.value === "" ||
+      copilotNameInput.value === "" ||
+      fuelLevelInput.value === "" ||
+      cargoMassInput.value === ""
     ) {
       alert("All fields are required!");
-      // stop the form submission
       event.preventDefault();
     }
 
-    if (isNaN(pilotName.value) == false || isNaN(copilotName.value) == false) {
-      alert("Text is required for Pilot and Co-Pilot");
-      event.preventDefault();
-    }
+    // Functions
   });
-});
 
-/* This block of code shows how to format the HTML once you fetch some planetary JSON!
-<h2>Mission Destination</h2>
-<ol>
-<li>Name: ${}</li>
-<li>Diameter: ${}</li>
-<li>Star: ${}</li>
-<li>Distance from Earth: ${}</li>
-<li>Number of Moons: ${}</li>
-</ol>
-<img src="${}">
-*/
+  //Mission Destination Information
+  let json = [];
+  fetch("https://handlers.education.launchcode.org/static/planets.json").then(
+    function(response) {
+      response.json().then(function(json) {
+        const missionTarget = document.getElementById("missionTarget");
+        missionTarget.innerHTML = `
+          <h2>Mission Destination</h2>
+            <ol>
+              <li>Name: ${json[1].name}</li>
+              <li>Diameter: ${json[1].diameter}</li>
+              <li>Star: ${json[1].star}</li>
+              <li>Distance from Earth: ${json[1].distance}</li>
+              <li>Number of Moons: ${json[1].moons}</li>
+            </ol>
+              <img src="${json[1].image}">
+  `;
+      });
+    }
+  );
+});
